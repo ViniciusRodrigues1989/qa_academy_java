72a2d75c13bbb91941bf0e1a95f9419ab7b816f4
ViniciusRodrigues1989 <vinicius.h.r33@gmail.com>
2022-08-16 19:20:31 -0300

    resolucao ex 5

--- src/main/java/medium/exercicio_05_medium.java
+++ src/main/java/medium/exercicio_05_medium.java
@@ -1,36 +1,20 @@
 package medium;
 
-import org.w3c.dom.ls.LSOutput;
-
 import javax.swing.*;
 
 public class exercicio_05_medium {
-    static double primeiroNumero, segundoNumero, terceiroNumero, quartoNumero, quintoNumero, menorNumero;
+    static int numeroDigitado, menorNumero,i = 1;
+    public static void main (String[]args){
+while (i <= 5){
+    numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digíte o" + i + "número"));
 
-    public static void main (String[]args) {
-        primeiroNumero = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro número"));
-        segundoNumero = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo número"));
-        terceiroNumero = Double.parseDouble(JOptionPane.showInputDialog("Digite o terceiro numero"));
-        quartoNumero = Double.parseDouble(JOptionPane.showInputDialog("Digite o quarto numero"));
-        quintoNumero = Double.parseDouble(JOptionPane.showInputDialog("Digite o quinto numero"));
+        if (numeroDigitado < menorNumero || i == 1) ;
+        menorNumero = numeroDigitado;
+}
+i++;
 
-        if (primeiroNumero < segundoNumero && primeiroNumero < terceiroNumero && primeiroNumero < quartoNumero && primeiroNumero < quintoNumero) //{
- //           primeiroNumero = menorNumero;
-   //     }
-            System.out.println("O menor número digitado é:"+ primeiroNumero);
-        if (primeiroNumero > segundoNumero && segundoNumero < terceiroNumero && segundoNumero < quartoNumero && segundoNumero < quintoNumero)//{
-          //  segundoNumero = menorNumero;
-   // }
-            System.out.println("O menor número digitado é:"+ segundoNumero);
-        if (primeiroNumero>terceiroNumero && segundoNumero > terceiroNumero && terceiroNumero < quartoNumero&& terceiroNumero<quintoNumero)// {
-        //    terceiroNumero = menorNumero;
-       // }
-            System.out.println("O menor número digitado é:"+ terceiroNumero);
-        if (primeiroNumero > quintoNumero && segundoNumero>quintoNumero && terceiroNumero>quartoNumero &&quartoNumero<quintoNumero)
-            System.out.println("O menor número digitado é:"+ quartoNumero);
-        else
-        {
-            System.out.println("O menor número digitado é:"+ quintoNumero);
-    }
+   System.out.println(" O numero:" + menorNumero + "é o menor numero");
+
+             }
 }
-}
+
---
+++ src/main/java/medium/exercicio_06_medium.java
@@ -0,0 +1,4 @@
+package medium;
+
+public class exercicio_06_medium {
+}
