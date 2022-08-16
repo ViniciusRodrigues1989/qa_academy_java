a6ef9d40f7ad27198e2e4d38760458566fa5765d
ViniciusRodrigues1989 <vinicius.h.r33@gmail.com>
2022-08-16 19:50:04 -0300

    resolucao ex 6

--- src/main/java/medium/exercicio_06_medium.java
+++ src/main/java/medium/exercicio_06_medium.java
@@ -1,4 +1,17 @@
 package medium;
 
 public class exercicio_06_medium {
+
+    static int fatorial,i;
+
+    public static void main (String[]args){
+        fatorial = 10;
+                i = fatorial;
+                while (i >1) {
+                    fatorial =fatorial*(i-1);
+                    i--;
+                    }
+
+        System.out.println("O fatorial do numero digitado é:" + fatorial);
+    }
 }
