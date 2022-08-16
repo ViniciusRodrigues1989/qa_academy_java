43dce12ac64505e5de0e9710b2a4ab7628288165
ViniciusRodrigues1989 <vinicius.h.r33@gmail.com>
2022-08-16 20:13:56 -0300

    resolucao ex 7

--- src/main/java/medium/exercicio_05_medium.java
+++ src/main/java/medium/exercicio_05_medium.java
@@ -6,7 +6,7 @@ public class exercicio_05_medium {
     static int numeroDigitado, menorNumero,i = 1;
     public static void main (String[]args){
 while (i <= 5){
-    numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digíte o" + i + "número"));
+    numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digíte o numero"i "));
 
         if (numeroDigitado < menorNumero || i == 1) ;
         menorNumero = numeroDigitado;
