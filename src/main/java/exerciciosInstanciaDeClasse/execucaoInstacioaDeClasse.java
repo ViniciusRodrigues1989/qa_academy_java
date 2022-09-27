f278efffc81fe515e9f1ab406b7a422b0cf9771c
ViniciusRodrigues1989 <vinicius.h.r33@gmail.com>
2022-09-26 22:26:56 -0300

    refactoring exercices2 exec inst classe ex

---
+++ src/main/java/exerciciosInstanciaDeClasse/execucaoInstacioaDeClasse.java
@@ -0,0 +1,12 @@
+/*package exemplosInstanciaDeClasse;
+
+public class Execucao {
+
+    public static void main(String[] args) {
+        ExemploInstanciaDeClasse exemploInstancia = new ExemploInstanciaDeClasse();
+
+        System.out.println(exemploInstancia.nomeDaClasse);
+        exemploInstancia.exibirNomeDaClasse();
+    }
+}
+*/
